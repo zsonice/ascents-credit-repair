81fdb9455f19d9a54964d0630369f4d6aedb5035
Aila <71922730+Wotalala@users.noreply.github.com>
2024-05-15 19:19:03 +0800

    added h1
    dfsdf

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
+    </x-slot> <h1>hh</h1>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
