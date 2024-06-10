0a9f8d543f036bf39bdeea3a1bd213d7859f5b8f
Aila <71922730+Wotalala@users.noreply.github.com>
2024-06-10 17:10:39 +0800

    chart ud

--- public/js/jscript.js
+++ public/js/jscript.js
@@ -22,34 +22,63 @@ const body = document.querySelector("body"),
             }
 
         });
+       
+      
+    
+     
+      
         var options = {
-            series: [{
-            name: 'series1',
-            data: [31, 40, 28, 51, 42, 109, 100]
-          }, {
-            name: 'series2',
-            data: [11, 32, 45, 32, 34, 52, 41]
-          }],
-            chart: {
-            height: 350,
-            type: 'area'
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'smooth'
-          },
-          xaxis: {
-            type: 'datetime',
-            categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-          },
-          tooltip: {
-            x: {
-              format: 'dd/MM/yy HH:mm'
+          series: [{
+          name: 'TEAM A',
+          type: 'area',
+          data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33]
+        }, {
+          name: 'TEAM B',
+          type: 'line',
+          data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43]
+        }],
+          chart: {
+          height: 350,
+          type: 'line',
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        fill: {
+          type:'solid',
+          opacity: [0.35, 1],
+        },
+        labels: ['Dec 01', 'Dec 02','Dec 03','Dec 04','Dec 05','Dec 06','Dec 07','Dec 08','Dec 09 ','Dec 10','Dec 11'],
+        markers: {
+          size: 0
+        },
+        yaxis: [
+          {
+            title: {
+              text: 'Series A',
             },
           },
-          };
-  
-          var chart = new ApexCharts(document.querySelector("#chart"), options);
-          chart.render();
+          {
+            opposite: true,
+            title: {
+              text: 'Series B',
+            },
+          },
+        ],
+        tooltip: {
+          shared: true,
+          intersect: false,
+          y: {
+            formatter: function (y) {
+              if(typeof y !== "undefined") {
+                return  y.toFixed(0) + " points";
+              }
+              return y;
+            }
+          }
+        }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+      
